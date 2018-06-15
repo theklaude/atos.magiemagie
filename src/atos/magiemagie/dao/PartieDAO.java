395a4be2d341d0f4ad48a5ff1054ef04e310f5ee
theklaude <theklaude@GSVI.formation.local>
2018-06-15 09:13:28 +0200

    Mapping OK.

---
+++ src/atos/magiemagie/dao/PartieDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atos.magiemagie.dao;
+
+import atos.magiemagie.entity.Joueur;
+import atos.magiemagie.entity.Partie;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author theklaude
+ */
+public class PartieDAO {
+
+    public List<Partie> listerPartieNonDemarrees() {
+
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        Query query = (Query) em.createQuery(""
+                + "SELECT p "
+                + "FROM Parie p"
+                + "EXCEPT"
+                + "SELECT p "
+                + "FROM Parie p"
+                + "     JOIN p.joueurs j "
+                + "WHERE j.etat IN (:etat_gagne,:etat_alamain)");
+               
+        query.setParameter("etat_gagne", Joueur.EtatJoueur.GAGNE);
+        query.setParameter("etat_alamain", Joueur.EtatJoueur.A_LA_MAIN);
+                
+                
+        return query.getResultList();
+    }
+
+}
